ac9bc882bffc097a4c3f8b9f6b75dbdee1946c2c
rememberlenny <rememberlenny@gmail.com>
2015-07-14 14:19:05 -0400

    Create the trending parsely search

--- app/controllers/parsely_controller.rb
+++ app/controllers/parsely_controller.rb
@@ -1,6 +1,8 @@
 class ParselyController < ApplicationController
   def search
-    return json: {status: 'success'}
+    response = HTTParty.get('https://api.parsely.com/v2/search?apikey=ENV['EDNA_PARSELY_KEY']&secret=ENV['EDNA_PARSELY_SECRET']&page=1&limit=5&sort=_hits&q=chick-fil-a&strategy=click&click.method=ref_search')
+    json = { body: response.body, code: response.code, message:response.message, header: response.headers.inspect}
+    render json: response
   end
 
   def save
