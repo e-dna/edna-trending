dd7b3164d373a995e51e34e3aadcdcd22fddc74b
rememberlenny <rememberlenny@gmail.com>
2015-07-14 17:44:22 -0400

    Add the realtime endpoint

--- app/controllers/parsely_controller.rb
+++ app/controllers/parsely_controller.rb
@@ -1,7 +1,14 @@
 class ParselyController < ApplicationController
   def search
-    params = 'apikey=' + ENV['EDNA_PARSELY_KEY'] + '&secret=' + ENV['EDNA_PARSELY_SECRET'] + '&page=1&limit=5&sort=_hits&q=' + keyword + '&strategy=click&click.method=ref_search'
+    params = 'apikey=' + ENV['EDNA_PARSELY_KEY'] + '&secret=' + ENV['EDNA_PARSELY_SECRET'] + '&page=1&limit=100&sort=_hits&q=' + keyword + '&strategy=click&click.method=ref_search'
     response = HTTParty.get('https://api.parsely.com/v2/search?' + params)
+    json = { body: response.body, code: response.code, message:response.message, header: response.headers.inspect}
+    render json: response
+  end
+
+  def realtime
+    params = 'apikey=' + ENV['EDNA_PARSELY_KEY'] + '&secret=' + ENV['EDNA_PARSELY_SECRET'] + 'page=1&limit=100&time=5m'
+    response = HTTParty.get('https://api.parsely.com/v2/realtime/posts?' + params)
     json = { body: response.body, code: response.code, message:response.message, header: response.headers.inspect}
     render json: response
   end
