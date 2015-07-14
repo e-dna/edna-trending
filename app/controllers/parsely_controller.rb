2f983870346ce767ae4504d65a75d7d61d22eba3
rememberlenny <rememberlenny@gmail.com>
2015-07-14 18:31:34 -0400

    Create the trending data

--- app/controllers/parsely_controller.rb
+++ app/controllers/parsely_controller.rb
@@ -1,12 +1,12 @@
 class ParselyController < ApplicationController
   def search
-    keyword = params[:keyword]
+    keyword = URI::encode(params[:keyword])
     monthsago = (Date.today - 6.months).strftime("%Y-%m-%d")
     reg = 'apikey=' + ENV['EDNA_PARSELY_KEY'] + '&secret=' + ENV['EDNA_PARSELY_SECRET']
     params = reg + '&pub_date_start=2000-01-01&pub_date_end=' + monthsago + '&page=1&limit=100&sort=_hits&q=' + keyword + '&strategy=click&click.method=ref_search'
     response = HTTParty.get('https://api.parsely.com/v2/search?' + params)
-    json = { body: response.body, code: response.code, message:response.message, header: response.headers.inspect}
-    render json: response
+    json = { query: URI::decode(keyword), status: 'success', response: response}
+    render json: json
   end
 
   def realtime
