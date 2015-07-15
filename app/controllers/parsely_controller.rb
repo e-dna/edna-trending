5a54de2f908512b5351c6c2b68898643fc241f98
rememberlenny <rememberlenny@gmail.com>
2015-07-15 10:23:48 -0400

    Save the logic

--- app/controllers/parsely_controller.rb
+++ app/controllers/parsely_controller.rb
@@ -3,7 +3,7 @@ class ParselyController < ApplicationController
     keyword = URI::encode(params[:keyword])
     monthsago = (Date.today - 6.months).strftime("%Y-%m-%d")
     reg = 'apikey=' + ENV['EDNA_PARSELY_KEY'] + '&secret=' + ENV['EDNA_PARSELY_SECRET']
-    params = reg + '&pub_date_start=2000-01-01&pub_date_end=' + monthsago + '&page=1&limit=100&sort=_hits&q=' + keyword + '&strategy=click&click.method=ref_internal'
+    params = reg + '&pub_date_start=2000-01-01&pub_date_end=' + monthsago + '&page=1&limit=100&sort=_hits&q=' + keyword + '&strategy=recency'
     response = HTTParty.get('https://api.parsely.com/v2/search?' + params)
     json = { query: URI::decode(keyword), status: 'success', response: response}
     render json: json
